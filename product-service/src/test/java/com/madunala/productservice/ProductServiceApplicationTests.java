3cd5c71472e8f5b6eb9f04c1457f434d4a030efb
Naresh Madunala <nareshkumar.madunala@gmail.com>
2024-03-27 12:44:30 -0700

    init of order product payment services

---
+++ product-service/src/test/java/com/madunala/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madunala.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
